5691f3ab8634a1021dddfe94d14ed21f260587e2
Calf79 <camerino_alfaro2@my.vcccd.edu>
2024-06-27 21:07:18 -0700

    Lab4-4-2 calf79

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    while True:
+        try:
+            Inpat= int(input('Enter Number'))
+            number= float(Inpat)
+        except ValueError:
+            print('Input must be numeric!')
+        else:
+            print(f'Valid input:{number}')
+            break
+        
     ########################################
     # Do not delete the return statement
     ########################################
